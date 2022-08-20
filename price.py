458a91ecc035e5ddb3a0d4657ec75663662c2195
IhorKush <ihoryakushev88@gmail.com>
2022-08-20 22:55:43 +0300

    first version

---
+++ price.py
@@ -0,0 +1,14 @@
+def discounted(price, discount, max_discounted=40):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discounted = abs(float(max_discounted))
+    if max_discounted > 99:
+        raise ValueError('The discount cannot be more than 99%')
+    if discount >= max_discounted:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount / 100
+    return price_with_discount
+
+
+print(discounted(100, 10, 30))
